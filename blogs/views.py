3cb887635f7a1f1df790ffc984f562730c97baab
Pubodin Tieanthong <oatxshot555@gmail.com>
2022-06-09 15:27:57 +0700

    v1.1

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Drop_sim
 # Create your views here.
 def drop_sim(request):
-    data = Drop_sim.objects.all()
-    print("Here")
+    #data = Drop_sim.objects.all()
+    norad_id = 51689
+    data = Drop_sim.objects.get(norad_id=norad_id)
+    
+    # formatDate = data.msg_epoch.strftime("%Y-%m-%d %H:%M:%S")
+    # print(formatDate)
     return render(request, "drop.html",{'data':data})
