b4020ce02b03c3038dd84b2ab0f7fd5290db5a4e
Pubodin Tieanthong <oatxshot555@gmail.com>
2022-07-21 09:25:06 +0700

    Done.

--- blogs/views.py
+++ blogs/views.py
@@ -23,6 +23,19 @@ def index(request):
 
 def match(request, objid=None):
     # then do whatever you want with your params
+    reentry_dates = []
+    datas = Drop_sim.objects.all().order_by('-decay_epoch')
+    for data in datas:
+        line_split = data.dataset.split('\r\n')
+        reentry_date = line_split[-1].split(" ")[0]
+        reentry_time = line_split[-1].split(" ")[1]
+        x = datetime(int(reentry_date.split("/")[0]), int(reentry_date.split("/")[1]), int(reentry_date.split("/")[2]),int(reentry_time.split(":")[0]),int(reentry_time.split(":")[1]),int(reentry_time.split(":")[2][:2]))
+        reentry_dates.append(x.strftime("%d %b %Y , %H:%M:%S UTC"))
+
+
+
+
+
     norad_id = objid
     data = Drop_sim.objects.get(norad_id=norad_id)
     print("Request ObjectID : " + str(objid))
@@ -61,6 +74,19 @@ def match(request, objid=None):
             uct_alt.append(0)
             uct_lat.append(float(splited[8]))
             uct_lon.append(float(splited[9]))
-
+    
+    context = {
+        'zip_datas': zip(datas, reentry_dates),
+        'totaldata':len(date),
+        'data':data ,
+        'date':date , 
+        'velocity':velocity ,
+        'height':height , 
+        'latitude':latitude , 
+        'longitude':longitude , 
+        'uct_lon':uct_lon, 
+        'uct_lat':uct_lat, 
+        'uct_alt':uct_alt 
+    }
     print("Success Request")
-    return render(request, "drop.html",{'totaldata':len(date),'data':data ,'date':date , 'velocity':velocity ,'height':height , 'latitude':latitude , 'longitude':longitude , 'uct_lon':uct_lon, 'uct_lat':uct_lat, 'uct_alt':uct_alt })
+    return render(request, "drop.html",context)
